be4e84cd63af7c724564a93c8a23a6d01a71883d
Paul Kirby <kirby.paulh@gmail.com>
2021-05-27 13:14:41 -0400

    Moved several repeated lines into new lookupNameSymbol function

--- binToXml.c
+++ binToXml.c
@@ -64,23 +64,25 @@ void processFF(fileStatus *fs, symbolMaps *sm) {
 	return;
 }
 
-// Called at first FF of two that indicates new symbol
+// Called at first FF of two that indicates new symbol, writes symbol to output file and returns 16-bit symbol array key
 uint16_t newSym(fileStatus *fs, symbolMaps *sm) {
 	char symbol[20];
 	fs->i += 2;									// Advance past two FF bytes
 	long x = conv32(fs->source[fs->i]);			// 4 bytes that represent symbol length in bytes
-	char *nameElem = malloc(x + 1);				// Name string that will be stored in name array
 	fs->i += 4;									// Advance past symbol length bytes
+	char *nameElem = malloc(x + 1);				// Name string that will be stored in name array
 	long j;
-	for (j = 0; j < x; j++) {					// Write letters to file
+	for (j = 0; j < x; j++) {					// Copy letters from source to temp array
 		nameElem[j] = fs->source[fs->i+j];
 	}
-	nameElem[j+1] = '\0';
-	fputs(nameElem, fs->outFile);
+	nameElem[j+1] = '\0';						// Add null terminator to string
+
+	fputs(nameElem, fs->outFile);				// Write the word to the outFile
+	fs->i += x;									// Increment i the length of the word
+
 	sm->symArray[sm->symArrayIdx] = nameElem;
 	sm->symArrayIdx++;
 	
-	fs->i += x;
 	return sm->symArrayIdx;
 }
 
@@ -92,19 +94,20 @@ uint32_t newElem(uint16_t nameSym, uint16_t attrSym) {
 	return elemKey;
 	}
 
+void writeElemBegin(fileStatus *fs, bool hasChildren)
+{
+	addTabs(fs);
+	if (hasChildren)
+		fs->tabPos++;								// Increment the tab count, because 0x50 always has children
+	fputc('<', fs->outFile);						// Write opening < char of element
+}
+
 // process50 is called when i is at the 0x50, and handles writing an FF50 element to the output file
 void process50(fileStatus *fs, symbolMaps *sm) {
 	uint16_t nameSym, attrSym = 0;					// 16-bit references to the symbol array
-	addTabs(fs);
-	fs->tabPos++;									// Increment the tab count, because 0x50 always has children
-	fputc('<', fs->outFile);						// Write opening < char of element
-	if (fs->source[fs->++i] == '\xFF') {			// If the byte after 0x50 is FF, this is a new symbol
-		nameSym = newSym(fs, sm);					// Save this new symbol in the table, return it's reference number
-	} else {										// If the symbol already exists in the symbol array
-		int arrIdx = conv16(fs->source[fs->i]);		
-		fs->i += 2;									// Move past 16-bit symbol array key 
-		fputs(sm->symArray[arrIdx], fs->outFile);	// Write the value at the symbol array to the xml file
-	}
+	writeElemBegin(fs, true);
+	lookupNameSymbol(fs, sm);
+
 	uint32_t id = conv32(fs->source[fs->i]);		// convert d:id attribute value to int
 	fs->i += 8; 									// Skip over rest of d:id attribute and # of children bytes
 	if (id == 0) {									// If zero value of d:id attribute, nothing is written
@@ -112,21 +115,28 @@ void process50(fileStatus *fs, symbolMaps *sm) {
 	} else {										// Else, write d:id and attribute value
 		fprintf(fs->outFile, " d:id=\"%u\">\n", id);
 	}
+
 	newElem(nameSym, attrSym);						// Save line in elemArray if < 255
 	return;
 }
 
-void process56(fileStatus *fs, symbolMaps *sm) {
-	addTabs(fs);
-	uint16_t nameSym, attrSym;
-	fputc('<', fs->outFile);
-	if (fs->source[fs->++i] == '\xFF') {
-		nameSym = newSym(fs, sm);
-	} else {
-		int arrIdx = conv16(fs->source[fs->i]);
-		fs->i += 2;
-		fputs(sm->symArray[arrIdx], fs->outFile);
+void lookupNameSymbol(fileStatus *fs, symbolMaps *sm)
+{
+	if (fs->source[fs->++i] == '\xFF') {			// If the byte after 0x50 is FF, this is a new symbol
+		nameSym = newSym(fs, sm);					// Save this new symbol in the table, return it's reference number
+	} else {										// If the symbol already exists in the symbol array
+		int arrIdx = conv16(fs->source[fs->i]);		
+		fs->i += 2;									// Move past 16-bit symbol array key 
+		fputs(sm->symArray[arrIdx], fs->outFile);	// Write the value at the symbol array to the xml file
 	}
+}
+
+void process56(fileStatus *fs, symbolMaps *sm) {
+	uint16_t nameSym, attrSym;
+
+	writeElemBegin(fs, false)
+	lookupNameSymbol(fs, sm);
+
 	fprintf(fs->outFile, " d:type=\"");
 	if (fs->source[fs->i] == '\xFF') {
 		attrSym = newSym(fs, sm);
@@ -135,21 +145,17 @@ void process56(fileStatus *fs, symbolMaps *sm) {
 		fs->i += 2;
 		fputs(sm->symArray[arrIdx], fs->outFile);
 	}
-	newElem(nameSym, attrSym);
 	fputs("\">\n", fs->outFile);
+
+	newElem(nameSym, attrSym);
 }
 
 void process41(fileStatus *fs, symbolMaps *sm) {
-	addTabs(fs);
 	uint16_t nameSym, attrSym = 0;		// TODO get attribute symbol
-	fputc('<', fs->outFile);
-	if (fs->source[fs->++i] == '\xFF') {
-		nameSym = newSym(fs, sm);
-	} else {
-		int arrIdx = conv16(fs->source[fs->i]);
-		fs->i += 2;
-		fputs(sm->symArray[arrIdx], fs->outFile);
-	}
+
+	writeElemBegin(fs, false);
+	lookupNameSymbol(fs, sm);
+
 	fputs(">\n", fs->outFile);
 	newElem(nameSym, attrSym);
 	return;
@@ -160,13 +166,9 @@ void process70(fileStatus *fs, symbolMaps *sm) {
 	uint16_t nameSym = 0, attrSym = 0;
 	addTabs(fs);
 	fputs("</", fs->outFile);
-	if (fs->source[fs->++i] == '\xFF') {
-		nameSym = newSym(fs, sm);
-	} else {
-		int arrIdx = conv16(fs->source[fs->i]);
-		fs->i += 2;
-		fputs(sm->symArray[arrIdx], fs->outFile);
-	}
+
+	lookupNameSymbol(fs, sm);
+
 	fputs(">\n", fs->outFile);
 	newElem(nameSym, attrSym);
 }
